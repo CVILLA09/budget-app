53700542738ab8ab7915d83d030aafb7385dfd46
Cristian Villa <c.villavirgen@gmail.com>
2023-12-01 04:06:07 -0600

    WIP: Fixing icon image functionality on categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   # Associations
   has_and_belongs_to_many :users
   has_and_belongs_to_many :purchases
+  has_one_attached :icon
 
   # Validations
   validates :name, presence: true
